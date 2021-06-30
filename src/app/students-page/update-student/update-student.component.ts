ffbae9b3038012cd617efa0f0a1c3f25b956c569
Luther <luther1096@gmail.com>
2021-07-01 03:09:28 +0530

    fixed minor bugs in succes alert message box

--- src/app/students-page/update-student/update-student.component.ts
+++ src/app/students-page/update-student/update-student.component.ts
@@ -6,7 +6,8 @@ import { StudentDataService } from '../students-data.service';
 @Component({
   selector: 'app-update-student',
   templateUrl: './update-student.component.html',
-  styleUrls: ['./update-student.component.css']
+  styleUrls: ['./update-student.component.css'],
+  
 })
 export class UpdateStudentComponent implements OnInit {
   model = new Student(105,'Anushka', 'Gurgaon', 'anu105@gmail.com', 'http://robohash.org/105/?set=set2',new Marks(75,65,33,85,90));
